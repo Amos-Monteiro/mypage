a526ff13ac69549f3b17240472f060c0ea241179
Amós Pedro <amos.pedro1@gmail.com>
2023-12-26 19:35:14 -0300

    .

--- my-portifolio/src/components/About.jsx
+++ my-portifolio/src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section className='about-conteiner'>
       <h2>Sobre</h2>
-      <p> Estudante de <b>ADS</b> (analise e desenvolvimento de sistemas) na <b>Uninassau</b>, atualmente atuando como Desenvilvedor-freelancer. </p>
+      <p> Estudante de <b>ADS</b> (analise e desenvolvimento de sistemas) na <b>Uninassau</b>, atualmente atuando como Desenvolvedor-freelancer. </p>
 
       <p> 
           Planejamento, comunicação, trabalho em equipe e organização  foram algumas das caracteristicas adiquiridas através do ambiente academico. Focado em me desenvolver e aprender cada dia mais.
--- my-portifolio/src/components/Projects.jsx
+++ my-portifolio/src/components/Projects.jsx
@@ -5,8 +5,6 @@ const Projects = () => {
     <section className="projects-container">
 
       <h2>Projetos</h2>
-      <p>Aqui tem ideias, atividades da faculdade e tutoriais de projetos.</p>
-
       <a href="https://github.com/Amos-Monteiro?tab=repositories" className="btn" target="blank">
         Ver Projetos
       </a>
