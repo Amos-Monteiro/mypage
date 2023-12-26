7ba046ff9f71ebb8f5884d2e4205cf30870f363b
Amós Pedro <amos.pedro1@gmail.com>
2023-12-26 19:22:12 -0300

    a

--- my-portifolio/src/components/SocialNetworks.jsx
+++ my-portifolio/src/components/SocialNetworks.jsx
@@ -3,9 +3,9 @@ import {FaLinkedinIn, FaGithub, FaInstagram } from 'react-icons/fa';
 import '../styles/components/socialnetwork.sass';
 
 const socialNetworks = [
-    {name: "linkedin", icon: <FaLinkedinIn/>, URL: "https://www.linkedin.com/feed/"},
+    {name: "linkedin", icon: <FaLinkedinIn/>, URL: "https://www.linkedin.com/in/am%C3%B3s-monteiro-91a643250/"},
     {name: "github", icon: <FaGithub/>,  URL:"https://github.com/Amos-Monteiro"},
-    {name:"instagram", icon: <FaInstagram/>,  URL: "https://www.instagram.com/"  },
+    {name:"instagram", icon: <FaInstagram/>,  URL: "https://www.instagram.com/amos.montero/" },
 ];
 
 const SocialNetworks = () => {
